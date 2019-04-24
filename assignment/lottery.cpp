91124c496661970ca83026d1072734942c3b41f0
DalexKraus <dalex.kraus@gmail.com>
2019-04-24 15:04:51 +0200

    In the past only god and me knew what this code did. Now only god knows.

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -15,11 +15,12 @@
 #include <string.h>
 #include "lottery.h"
 
-#define 	UUID_LEN       40
-#define 	MAX_TIP_LEN    17
-#define 	MAX_LINE_LEN   (UUID_LEN + 1 + MAX_TIP_LEN + 1)
+#define 	UUID_LEN       36
+#define 	MAX_TIP_LEN    19
+#define 	MAX_LINE_LEN   (UUID_LEN + 1 + MAX_TIP_LEN)
 
-bool contains(int number, int numbers[TIP_SIZE]);
+int get_file_length(FILE* fd);
+int get_line_length(FILE* fd, int line_index);
 
 FILE* fd;
 char separator;
@@ -33,8 +34,13 @@ bool init_lottery(const char *csv_file, char csv_separator)
 
 bool get_tip(int tip_number, int tip[TIP_SIZE])
 {
+    if (tip_number < 0 || get_file_length(fd) < (tip_number * MAX_LINE_LEN))
+        return false;
+
+    printf("Tip no. %d, pos: %d\n", tip_number, get_line_length(fd, tip_number));
+
     fseek(fd, MAX_LINE_LEN * tip_number, SEEK_SET);
-    fseek(fd, UUID_LEN, SEEK_CUR);
+    fseek(fd, UUID_LEN + 1, SEEK_CUR);
 
     char tipString[MAX_TIP_LEN];
     fgets(tipString, MAX_TIP_LEN, fd);
@@ -44,10 +50,9 @@ bool get_tip(int tip_number, int tip[TIP_SIZE])
     ptr = strtok(tipString, delimiter);
 
     int pos = 0;
-    while(ptr != NULL) {
-        if (!contains(atoi(ptr), tip))
-            return false;
-
+    while (ptr != NULL)
+    {
+        tip[pos] = atoi(ptr);
         ptr = strtok(NULL, delimiter);
         pos++;
     }
@@ -69,12 +74,43 @@ int get_right_tips_count(int right_digits_count)
     return 0;
 }
 
-bool contains(int number, int numbers[TIP_SIZE])
+int get_file_length(FILE* fd)
 {
-    for (int i = 0; i < TIP_SIZE; i++)
+    int previousPos = ftell(fd);
+    fseek(fd, 0, SEEK_END);
+    int end = ftell(fd);
+    fseek(fd, previousPos, SEEK_SET);
+    return end;
+}
+
+int get_line_length(FILE* fd, int line_index)
+{
+    int previousPos = ftell(fd);
+
+    //Jump to target line start
+    int pos = 1;
+    fseek(fd, 0, SEEK_SET);
+    while ((pos - 1) < line_index)
     {
-        if (numbers[i] == number)
-            return true;
+        while (fgetc(fd) != '\n')
+        {
+            fseek(fd, 1, SEEK_CUR);
+        }
+        printf("Char: %c", (char) fgetc(fd));
+
+        pos++;
     }
-    return false;
+    printf("Line Pos: %d\n", pos);
+
+    //Run through line until \n
+    int length = 0;
+    while (fgetc(fd) != '\n')
+    {
+        fseek(fd, 1, SEEK_CUR);
+        length++;
+    }
+
+    //Jump to previous pos
+    fseek(fd, previousPos, SEEK_SET);
+    return length;
 }
