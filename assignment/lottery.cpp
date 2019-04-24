475990672aba0c30cc0abe5461203d6be1baedbc
Dalex <Dalex@DESKTOP-C0SOJHB>
2019-04-24 16:29:41 +0200

    ?Implemented get_tip

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -16,7 +16,9 @@
 #include "lottery.h"
 
 #define 	UUID_LEN       36
-#define 	MAX_TIP_LEN    19
+#define 	MAX_TIP_LEN    17
+
+//Unused?
 #define 	MAX_LINE_LEN   (UUID_LEN + 1 + MAX_TIP_LEN)
 
 int get_file_length(FILE* fd);
@@ -34,27 +36,42 @@ bool init_lottery(const char *csv_file, char csv_separator)
 
 bool get_tip(int tip_number, int tip[TIP_SIZE])
 {
-    if (tip_number < 0 || get_file_length(fd) < (tip_number * MAX_LINE_LEN))
+    //Validate tip number
+    if (tip_number < 0 || tip_number >= 1000000)
         return false;
 
-    printf("Tip no. %d, pos: %d\n", tip_number, get_line_length(fd, tip_number));
+    //Jump to tip
+    //Iterate to target line start (can't use fseek() because each line is differently long)
+    rewind(fd);
+    int currentLine = 0; //Representing the current line in file
+    while (currentLine < tip_number)
+        for (;;) {
+            if (fgetc(fd) == '\n') {
+                currentLine++;
+                break;
+            }
+        }
 
-    fseek(fd, MAX_LINE_LEN * tip_number, SEEK_SET);
+    //Skip UUID (+first separator)
     fseek(fd, UUID_LEN + 1, SEEK_CUR);
 
+    //Read tip string
     char tipString[MAX_TIP_LEN];
     fgets(tipString, MAX_TIP_LEN, fd);
 
+    printf("\n\nTip-String: %s\n\n", tipString);
+
+    //Split tip string
+    char delimiter[2] = { separator, '\0' };
     char *ptr;
-    char delimiter[] = { separator };
     ptr = strtok(tipString, delimiter);
 
     int pos = 0;
-    while (ptr != NULL)
-    {
-        tip[pos] = atoi(ptr);
+    while(ptr != NULL) {
+        int tipNumber = atoi(ptr);
+        tip[pos] = tipNumber > 0 && tipNumber <= 45 ? tipNumber : tip[pos];
+        printf(" tip: %d", tip[pos]);
         ptr = strtok(NULL, delimiter);
-        pos++;
     }
     return true;
 }
@@ -85,30 +102,14 @@ int get_file_length(FILE* fd)
 
 int get_line_length(FILE* fd, int line_index)
 {
-    int previousPos = ftell(fd);
-
-    //Jump to target line start
-    int pos = 1;
-    fseek(fd, 0, SEEK_SET);
-    while ((pos - 1) < line_index)
-    {
-        while (fgetc(fd) != '\n')
-        {
-            fseek(fd, 1, SEEK_CUR);
-        }
-        printf("Char: %c", (char) fgetc(fd));
-
-        pos++;
-    }
-    printf("Line Pos: %d\n", pos);
-
-    //Run through line until \n
+    int previousPos = ftell(fd);    //Backup previous pos
     int length = 0;
-    while (fgetc(fd) != '\n')
-    {
-        fseek(fd, 1, SEEK_CUR);
+    rewind(fd);        //Jump to start of file
+
+    //We've reached the point where we are before the line we want to count.
+    //Now determine length of line
+    while (!feof(fd) && fgetc(fd) != '\n')
         length++;
-    }
 
     //Jump to previous pos
     fseek(fd, previousPos, SEEK_SET);
